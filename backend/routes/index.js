3cb0f34964d6b9cad1bbc401c83b9b854876f33c
Wayne Jones <waynejones@outlook.com>
2020-01-15 17:56:51 -0500

    Fixed some router changes, Added Morgan

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = () => {
-    router.get('/', function(req, res) {
-        return res.send('Hello World');
-    });
+router.get('/', (req, res, next) => {
+  res.send('Hello World');
+});
 
-    return router;
-};
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,31 @@
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
 const mysql = require('mysql');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
 
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+
 app.get('env');
 
 app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 
+app.use('/', indexRouter);
 app.use(express.static('../public'));
 
-const routes = require('./routes');
-
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'test'
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test',
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
